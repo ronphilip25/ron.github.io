31f33770018eca27855cdb119d678c43bb8f1dda
ronlip2571 <sanchezronphilip@gmail.com>
2023-01-25 17:15:38 +0800

    First commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,23 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "D:\\Projects\\project\\app\\page": [
+      "D:\\Projects\\project\\app\\globals.css"
+    ],
+    "D:\\Projects\\project\\app\\Skills\\page": [
+      "D:\\Projects\\project\\app\\globals.css"
+    ],
+    "D:\\Projects\\project\\app\\About\\page": [
+      "D:\\Projects\\project\\app\\globals.css"
+    ],
+    "D:\\Projects\\project\\app\\Contacts\\page": [
+      "D:\\Projects\\project\\app\\globals.css"
+    ]
+  },
+  "D:\\Projects\\project\\app\\page.jsx": [
+    "D:\\Projects\\project\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"app\\\\page.jsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+    "D:\\Projects\\project\\app\\page.module.css"
+  ],
+  "D:\\Projects\\project\\app\\layout.jsx": [
+    "D:\\Projects\\project\\app\\globals.css"
+  ]
+}
---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,8 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {},
+  "app": {
+    "D:\\Projects\\project\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"app\\\\page.jsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/2aaf0723e720e8b9.p.woff2"
+    ]
+  }
+}
